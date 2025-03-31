c91976c6f1ce5abf73af730610d4a7e836657838
Cayleeeee <155513661+Cayleeeee@users.noreply.github.com>
2025-03-31 21:19:48 +0800

    Update SchoolData.kt

--- admin/src/main/java/com/example/admin/SchoolData.kt
+++ admin/src/main/java/com/example/admin/SchoolData.kt
@@ -1,6 +1,7 @@
 package com.example.admin
 
 data class SchoolData(
-    val school:String? = null,
-    val languages:String? = null,
-    val website:String? = null)
+    val school: String? = "",
+    val languages: String? = "",
+    val website: String? = ""
+)
